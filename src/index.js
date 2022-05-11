a169250a3751b7ebd838ccc8b381bd4dda6a8b47
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-05-11 15:27:01 +0200

    question 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser');
 const PORT = 3000  
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+//Error Handling
+app.use((req, res, next) => {
+    const error = new Error("Route not found.");
+    error.status = 404;
+    next(error)
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 app.get('/', (req, res) => {
     return res.send("Hello word");
